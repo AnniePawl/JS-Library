0fe6d79182edcfc58e584b664c037afd20af7216
Anna Pawl <annampawl@gmail.com>
2019-10-22 14:53:35 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Goal: Uppercase each letter in a string
+
+// Check 1st character to confirm it is actual letter
+
+// Split on space (tab, line return)
+
+// Uppercase string 
+
+// Merge Strings
