00a929b0262699a548ddb3401e82f6aabe198757
Anna Pawl <annampawl@gmail.com>
2019-11-03 14:01:05 -0800

    adding new tests

--- index.js
+++ index.js
@@ -1,97 +1,43 @@
-// CAPITALIZE FIRST WORD
+// Capitalize first word 
 String.prototype.capitalizeFirst = function () {
-
-  // const charOne = this[0]
-  // const upperFirst = charOne.toUpperCase()
-  // const leftOverCharacters = this.slice(1)
-  // const joinedCharacters = upperFirst + leftOverCharacters
-  // return joinedCharacters
   return this[0].toUpperCase() + this.slice(1)
 }
-console.log("foo bar".capitalizeFirst())
 
+// Capitalize All Words
+// Edge Case- Extra spaces between words?
+String.prototype.captitalizeAll = function () {
+  return this.split(' ').map((word) => {
+    return word[0].toUpperCase() + word.slice(1)
+  }).join(' ')
+}
 
-
-// TRANSFORM TO UPPERCASE
+// UPPERCASE ALL
+// Edge Cases?
 String.prototype.upperCase = function () {
   return this.toUpperCase()
 }
-console.log("foo bar".upperCase())
 
-
-// Capitalize All Words
-String.prototype.allCaps = function () {
-  return this.split(' ').map((word) => {
-    // Check to see if word is "small word"
-
-    return word.capitalizeFirst()
-  }).join(' ')
-
-  // const words = this.split(' ')
-  // const allCapWords = words.map((word) => {
-  //   return word.capitalizeFirst()
-  // })
-  // const joinedWords = allCapWords.join(' ')
-
-  // return joinedWords
-}
-console.log("foo bar".allCaps())
-
-// Remove Extra Space
+// Remove Spaces 
+// Edge Cases?
 String.prototype.removeSpace = function () {
   return this.replace(/\s/g, "")
-
-}
-console.log("  foo   bar ".removeSpace())
-
-
-// Remove Extra Space Alternative
-String.prototype.removeSpace2 = function () {
-  // "a frog"       ['a', '  frog  ']
-  return this.split(' ').map((word) => { return word.trim() }).join('')
 }
 
-console.log('------------')
-console.log(" aaa foo moo  bar ".removeSpace2())
-
-
-
-// KABOB CASE
+// kabob-case
+// Edge Cases
 String.prototype.kabobCase = function () {
-  // Lowercase, split on space, trim, join with -
-  const t = this
-    .toLowerCase()
-    .split(" ")
-
-  console.log('')
-  console.log(t)
-  console.log('')
-
-  return this
-    .toLowerCase()
-    .split(" ")
-    // .map((word) => word.trim())
-    .filter((word) => word != '')
-    .join('-')
-
-  // return this.removeSpace().toLowerCase().split(' ').join('-')
-
-  // ' '  >  '' > -''-
+  return this.toLowerCase().split(' ').join('-')
 }
-console.log("  foo  Kabob  \n  Bar \t r ".kabobCase())
 
-
-// SNAKE CASE
+// Snake Case
+// Edge Cases? 
 String.prototype.snakeCase = function () {
-  return this.split(' ').join('_')
+  return this.toLowerCase().split(' ').join('_')
 }
-console.log("foo   Bar".snakeCase())
+console.log('snake case me'.snakeCase())
 
 
-// CAMEL CASE
-// String.prototype.CamelCase = function () {
-//   return this.split(' ').
-
-// }
-// console.log("foo bar".CamelCase())
-
+// CamelCase
+String.prototype.camelCase = function () {
+  return
+}
--- tests/test.js
+++ tests/test.js
@@ -2,34 +2,62 @@ require('../index')
 
 // Capitalize First Letter Test 
 test('Capitalize First Letter', () => {
-
+  expect('hello'.capitalizeFirst()).not().toBe('hello')
+  expect('hello'.capitalizeFirst()).toBe('Hello')
+  expect('good morning'.capitalizeFirst().toBe('Good morning'))
 })
 
 // All Caps Test
 test('All Caps Test', () => {
+  expect('good morning'.capitalizeAll()).toBe('Good Morning')
+  expect('good-morning'.capitalizeAll()).toBe('Good-Morning')
+  expect('good  morning'.capitalizeAll()).toBe('Good  Morning')
+  expect(' good  morning'.capitalizeAll()).toBe(' Good  Morning')
 
 })
 
+// UPPERCASE TEST
+test('Upper Case Test', () => {
+  expect('uppercase'.upperCase()).toBe('UPPERCASE')
+  expect(' upper  case  '.upperCase()).toBe('UPPER CASE')
+})
 
 // Remove Extra Space Test
+// Checks for front/back white space, tab, new line, 
 test('Remove Space Test', () => {
+  expect('good  morning'.removeSpace()).toBe(' goodmorning')
+  expect('  goodmorning  '.removeSpace()).toBe(' goodmorning')
+  expect(' good  /t morning'.removeSpace()).toBe(' goodmorning')
+  expect(' good /n morning'.removeSpace()).toBe(' goodmorning')
 
 })
 
 
 // Kabob Case Test 
-test('Kabob Case Test', () => {
-
+test('Kabob Cast Test', () => {
+  expect('kabob me'.kababCase()).toBe('kabob-me')
+  expect(' kabob me '.kabobCase()).toBe('kabob-me')
+  expect('kabob   me'.kabobCase()).toBe('kabob-case')
 })
 
 
 // Snake Case Test 
 test('Snake Case', () => {
-
+  expect('snake case'.snakeCase()).toBe('snake_case')
+  expect('Snake Case'.snakeCase()).toBe('snake_case_me')
+  expect(' snake case me '.snakeCase()).toBe('snake_case')
+  expect('snake   case'.snakeCase()).toBe('snake_case')
+  expect('snake   /ncase'.snakeCase()).toBe('snake_case')
+  expect('snake   /t case '.snakeCase()).toBe('snake_case')
 })
 
 
 // Camel Case Test 
 test('Camel Case Test', () => {
+  expect('camel case'.camelCase().toBe('camelCase'))
+  expect('  camel case  '.camelCase()).toBe('camelCase')
+  expect('camel    case'.camelCase()).toBe('camelCase')
+  expect('camel /t case'.camelCase()).toBe('camelCase')
+  expect('camel /n case'.camelCase()).toBe('camelCase')
 
 })
