6a5a6bde9f63ce383c36f1e17f34c7cd8075f438
Anna Pawl <annampawl@gmail.com>
2019-11-12 15:06:30 -0800

    minified

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,21 @@
-export default {
+import { terser } from 'rollup-plugin-terser';
+
+export default [{
   input: 'src/index.js',
+  plugins: [terser()],
   output: {
     file: 'umd/anna-string-library.js',
     format: 'umd',
-    name: 'annaStringLibrary'
+    name: 'annaStringLibrary',
+    esModule: false,
   }
-};
+},
+
+{
+  input: 'src/index.js',
+  output: {
+    file: 'esm/index.js',
+    format: 'esm'
+  }
+}
+]
--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
-require('../index')
+require('../src/index')
 
 // capitalizeFirst() Test 
 test('capitalizeFirst', () => {
