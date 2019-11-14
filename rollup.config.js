c050628d541befa30170508daf6e5f814676c3c5
Anna Pawl <annampawl@gmail.com>
2019-11-14 15:03:55 -0800

    adding typescript, rebundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,25 @@
 import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
 export default [{
-  input: 'src/index.js',
-  plugins: [terser()],
+  input: 'src/index.ts',
+  plugins: [
+    terser(),
+    typescript(
+      {
+        typescript: require('typescript')
+      }
+    )
+  ],
+
   output: {
     file: 'umd/anna-string-library.js',
     format: 'umd',
     name: 'annaStringLibrary',
     esModule: false,
   }
+
 },
 
-{
-  input: 'src/index.js',
-  output: {
-    file: 'esm/index.js',
-    format: 'esm'
-  }
-}
+
 ]
